89044703824b73fde4a1401215248084dd19b778
Eme112 <eme_pedraza@hotmail.com>
2021-09-15 08:02:50 -0500

    Added test file

---
+++ TestCuriosity.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TestCuriosity.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TestCuriosity.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testcuriosity.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testcuriosity.x.tar
