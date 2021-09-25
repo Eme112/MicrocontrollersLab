d39c8e5efdedde8e27a8988253c79d2310bd592b
Eme112 <eme_pedraza@hotmail.com>
2021-09-25 01:47:58 -0500

    Created Tarea4

---
+++ Tarea4.X/LCD.asm
@@ -0,0 +1,99 @@
+    #include "p18f45k50.inc"
+    processor 18f45k50 	; (opcional).
+    org 0x00
+    goto PORTS
+    org 0x08 		; posición para interrupciones.
+    retfie
+    org 0x18		; posición para interrupciones.
+    retfie
+    org 0x30 		; Origen real (opcional pero recomendado).
+PORTS
+    #define RS LATB, 0, A	; RB0 -> Register Select.
+    #define E LATB, 1, A	; RB1 -> Enable.
+    #define RW LATB, 2, A	; RB2 -> Read/Write
+    #define dataLCD LATD, A
+; ETIQUETAS
+var1	equ 0x10
+var2	equ 0x11
+var3	equ 0x12
+CONFIGURE
+    movlw	.15
+    clrf	ANSELB, BANKED	; REGB -> Digital. 
+    clrf	ANSELD, BANKED	; REGD -> Digital.
+    clrf	TRISB, A	; REGB -> Output.
+    clrf	TRISD, A	; REGD -> Output.
+    org	0x40		; Código principal.
+START	
+    bcf	    E
+    call    delay100ms
+    movlw   b'00111000'		; 8-bit, 2 lines, 5x7.
+    call    INSTRUCTION_WRITE
+    movlw   b'00001111'		; Display on, cursor on, blynk on.
+    call    INSTRUCTION_WRITE
+    movlw   b'00000111'		; Increment cursor position, diplay shift.
+    call    INSTRUCTION_WRITE
+    movlw   b'00000001'		; Clear display and return to home position.
+    call    INSTRUCTION_WRITE
+WRITING
+    movlw   b'11000101'		; Move cursor to 0x45.
+    call    INSTRUCTION_WRITE
+    movlw   'c'			; Write a 'c'.
+    call    DATA_WRITE
+    movlw   '4'			; Write a '4'.
+    call    DATA_WRITE
+    
+INFINITE    goto    INFINITE	; Infinite loop.
+    
+INSTRUCTION_WRITE
+    bcf	    RS			; RS -> 0
+    bcf	    RW			; RW -> 0
+    goto    SEND_DATA
+DATA_WRITE
+    bsf	    RS			; RS -> 1
+    bcf	    RW			; RW -> 0
+    goto    SEND_DATA
+DATA_READ
+    bsf	    RS			; RS -> 1
+    bsf	    RW			; RW -> 1
+    goto    SEND_DATA
+SEND_DATA
+    bsf	    E			; Enable.
+    movwf   dataLCD		; Data transmission.
+    nop				; Wait 1us to make sure the instruction was recieved.
+    bcf	    E			; Stop Enable.
+    clrf    dataLCD
+    call    esperaLCD		; Wait to see if LCD is done.
+    return
+    
+esperaLCD
+    bcf	    RS			; RS -> 0
+    bsf	    RW			; RW -> 1
+    call    delay100us		; Wait 100us.
+    btfsc   PORTD, 7, A		; Read bussy flag.
+    goto    esperaLCD		; If set, keep waiting.
+    return			; If not set, return.
+    
+delay100us
+    movlw   .33
+    movwf   var1
+loop1				
+    decfsz  var1
+    goto    loop1
+    ; 100us approx.
+    return
+
+delay100ms
+    movlw   .0			; 256
+    movwf   var1
+    movlw   .128
+    movwf   var2
+loop2				
+    decfsz  var1
+    goto    loop2
+    decfsz  var2
+    goto    loop2
+    ; 100ms approx.
+    return
+	end
+	
+; Duda: Como leer busy flag si es una salida, no una entrada.
---
+++ Tarea4.X/build/default/production/LCD.o.d
@@ -0,0 +1,3 @@
+build/default/production/LCD.o:  \
+	C:/MicrochipProjects/Tarea4.X/LCD.asm  \
+
